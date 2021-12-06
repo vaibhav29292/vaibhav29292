2f40ff2eb67737231f9002238725f123eb092fb3
vaibhav <vaibhav29292@gmail.com>
2021-12-06 22:56:19 +0530

    new files

---
+++ src/main/java/com/crm/qa/util/Demo.java
@@ -0,0 +1,11 @@
+package com.crm.qa.util;
+
+public class Demo {
+
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+System.out.println("vabhav");
+System.out.println("poonam");
+	}
+}
